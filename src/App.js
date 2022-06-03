86db56877a4ce17c9db98902846783e395707e4c
tgithubk <pianizm@hotmail.com>
2022-06-03 11:11:51 +0900

    add reducer

--- src/App.js
+++ src/App.js
@@ -1,19 +1,46 @@
 import logo from './logo.svg';
 import './App.css';
-import Apifetch from './components/Apifetch';
+// import Apifetch from './components/Apifetch';
+import AppContext from './contexts/AppContext';
+// import B from './components/B';
+// import BasicReducer from './components/BasicReducer';
+import CompB from './components/CompB';
+
+import {useReducer} from 'react'
+
+const initialState = 0
+const reducer = (currentState, action) => {
+  switch(action) {
+    case 'add_1':
+      return currentState + 1
+    case 'multiple_3':
+      return currentState * 3
+    case 'reset':
+      return initialState
+    default:
+      return currentState
+  }
+}
 
 function App() {
+  const [count, dispatch] = useReducer(reducer,initialState)
   return (
+    <AppContext.Provider value={{countProvided:count,dispatchProvided:dispatch}}>
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        
+{/*         
         <Apifetch />
+        <B /> */}
+        {/* <BasicReducer /> */}
+        Count {count}
+        <CompB />
       </header>
     </div>
+    </AppContext.Provider>
   );
 }
 
